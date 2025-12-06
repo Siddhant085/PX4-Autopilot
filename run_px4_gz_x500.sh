034d0e9804ac7da029e36a37e68cbcb85a54f272
Tejas Mahajan <tejas.mahajan121@gmail.com>
2025-12-06 09:15:34 +0000

    Add gz world

--- run_px4_gz_x500.sh
+++ run_px4_gz_x500.sh
@@ -14,10 +14,11 @@ BUILD_DIR="$ROOT_DIR/build/px4_sitl_default"
 # 1) Start PX4 SITL (gz_x500 airframe)
 cd "$BUILD_DIR"
 source rootfs/gz_env.sh
+export PX4_GZ_WORLD=agriculture_world
 export PX4_SYS_AUTOSTAR=4014
 export PX4_INSTANCE=1
 export PX4_SIM_MODEL=gz_x500_mono_cam_down
-./bin/px4  -i $PX4_INSTANCE 
+./bin/px4  -i $PX4_INSTANCE
 PX4_PID=$!
 
 # 2) Start Gazebo (gz) with the x500 world
